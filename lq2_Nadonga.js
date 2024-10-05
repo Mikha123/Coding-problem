de931663a73646e72dc6045dfaef8e3071dd11bb
Mikhaella Nadonga <soriaomikhaella@gmail.com>
2024-10-05 13:34:55 +0800

    Add files via upload

---
+++ lq2_Nadonga.js
@@ -0,0 +1,29 @@
+//create a program must ask user for an input.
+//use window prompt() and alert().
+
+let userName = prompt("Enter your name:");
+let userAddress = prompt("Enter your address:");
+let userAge = prompt("Enter your age:");
+let classRoles = prompt("Enter your class roles:");
+let userCourse = prompt("Enter your course:");// the program must utilize a conditional statement that detects the course.
+//for each course they must have another conditional statement.
+
+
+if (userCourse === "BSCS"){ 
+  alert("BSCS");
+} else if (userCourse === "BSM" ){
+  alert("BSM");
+}else if (userCourse3 === "BAEL" ){
+  alert("BAEL");
+
+}else{
+  alert("non existent");
+}
+
+// where as each of class role must have loopstatement which will print the name of user.
+//the number of iterations depends on the 1/4 of the age input.
+ 
+let roles = classRoles;
+for (let i=0;i<classRole.length; i++) {
+  
+}
